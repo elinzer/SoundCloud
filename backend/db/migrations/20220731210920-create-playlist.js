361e9d06c5f6bca4547d5bd2bd7858bfbf4ff438
El Linzer <elinzer5@gmail.com>
2022-07-31 16:17:49 -0500

    added playlist and playlistSong table

---
+++ backend/db/migrations/20220731210920-create-playlist.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Playlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true
+      },
+      imgUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Playlists');
+  }
+};
