733034424de74c4cd25a015cf32f84e01072dd42
El Linzer <elinzer5@gmail.com>
2022-08-02 14:55:43 -0500

    added edit song route

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 //get all songs by current user
 router.get('/current', restoreUser, requireAuth, async (req, res) => {
-    const {user} = req;
+    const { user } = req;
 
     if (user) {
         const userSongs = await Song.findAll({
@@ -34,7 +34,12 @@ router.get('/:songId', async (req, res) => {
     const songId = req.params.songId;
 
     const song = await Song.findByPk(songId, {
-        include: [Album, User]
+        include: [
+            { model: User },
+            {
+                model: Album,
+                attributes: ['id', 'title', 'imgUrl']
+            }]
     })
     if (song) {
         res.json(song)
@@ -43,11 +48,9 @@ router.get('/:songId', async (req, res) => {
         res.json({
             "message": "Song couldn't be found",
             "statusCode": 404
-          })
+        })
     }
 })
-
-
 
 
 //get all songs
@@ -59,7 +62,52 @@ router.get('/', async (req, res) => {
     })
 })
 
+//edit a song
+router.put('/:songId', requireAuth, async (req, res) => {
+    const {userId, albumId, title, description, url, createdAt, updatedAt, imgUrl} = req.body;
 
+    const user = req.user.id;
+
+    console.log(user)
+
+    const song = await Song.findByPk(req.params.songId);
+
+    if (!song) {
+        res.statusCode = 404;
+        res.json({
+            "message": "Song couldn't be found",
+            "statusCode": 404
+          })
+    } else if (!title || !url || title === '' || url === '') {
+        res.statusCode = 400;
+        res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+              "title": "Song title is required",
+              "url": "Audio is required"
+            }
+          })
+    } else if (user !== song.userId) {
+        res.statusCode = 403;
+        res.json({
+            "message": 'This is not your song to edit!',
+            "statusCode": 403
+        })
+    } else if (user === song.userId) {
+        song.set({
+            title: title,
+            description: description,
+            url: url,
+            imgUrl: imgUrl
+        })
+        await song.save();
+
+        res.json(song)
+    }
+
+
+})
 
 
 
