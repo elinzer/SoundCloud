be6bc95c75e9cd271979e3158457192720455624
El Linzer <elinzer5@gmail.com>
2022-08-03 12:56:18 -0500

    worked on more album routes

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -20,6 +20,33 @@ router.get('/current', requireAuth, async (req, res) => {
     })
 })
 
+//get album details from id
+router.get('/:albumId', async (req, res) => {
+    const albumId = req.params.albumId;
+
+    const album = await Album.findByPk(albumId, {
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username']
+            },
+            {
+                model: Song,
+                attributes: ['id', 'userId', 'albumId', 'title', 'description', 'url', 'imageUrl', 'createdAt', 'updatedAt']
+            }]
+    })
+
+    if (!album) {
+        res.statusCode = 404;
+        res.json({
+            "message": "Album couldn't be found",
+            "statusCode": 404
+        })
+    } else {
+        res.json(album)
+    }
+
+})
 
 //get all albums
 router.get('/', async (req, res) => {
@@ -72,16 +99,85 @@ router.post('/:albumId/songs', requireAuth, async (req, res) => {
             imageUrl: imageUrl,
             albumId: albumId
         })
-
+        res.statusCode = 201;
         res.json(newSong)
     }
 
 })
 
+//create an album
+router.post('/', requireAuth, async (req, res) => {
+    const userId = req.user.id;
+    const { title, description, imageUrl } = req.body;
+
+    if (!title || title === '') {
+        res.statusCode = 400;
+        res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+                "title": "Album title is required"
+            }
+        })
+    } else {
+
+        const newAlbum = await Album.create({
+            userId: userId,
+            title: title,
+            description: description,
+            imageUrl: imageUrl
+        })
+        res.statusCode = 201
+        res.json(newAlbum)
+    }
+
+})
+
+
+//edit an album
+router.put('/:albumId', requireAuth, async (req, res) => {
+    const userId = req.user.id;
+
+    const {title, description, imageUrl} = req.body;
+
+    const albumToEdit = await Album.findByPk(req.params.albumId);
+
+    if (!title || title === '') {
+        res.statusCode = 400;
+        res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+              "title": "Album title is required"
+            }
+          })
+    } else if (!albumToEdit) {
+        res.statusCode = 404,
+        res.json({
+            "message": "Album couldn't be found",
+            "statusCode": 404
+          })
+    } else if (userId !== albumToEdit.userId) {
+        res.statusCode = 403;
+        res.json({
+            "message": 'This is not your album to edit!',
+            "statusCode": 403
+        })
+    } else {
+        albumToEdit.set({
+            title: title,
+            description: description,
+            imageUrl: imageUrl
+        })
+
+        await albumToEdit.save();
+
+        res.json(albumToEdit)
+    }
 
 
 
-
+})
 
 
 module.exports = router;
