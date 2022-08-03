9c326ea41581d3b9e1be2e8af434b93019a0394c
El Linzer <elinzer5@gmail.com>
2022-08-03 18:34:46 -0500

    create a playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -9,7 +9,33 @@ const { Op } = require("sequelize")
 const router = express.Router();
 
 
+//create a playlist
+router.post('/', requireAuth, async (req, res) => {
+    const userId = req.user.id;
 
+    const { name, imageUrl } = req.body;
+
+    if (!name || name === '') {
+        res.statusCode = 400;
+        res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+              "name": "Playlist name is required"
+            }
+          })
+    } else {
+        const newPlaylist = await Playlist.create({
+            userId: userId,
+            name: name,
+            imageUrl: imageUrl
+        })
+
+        res.statusCode = 201;
+        res.json(newPlaylist)
+    }
+
+})
 
 
 
