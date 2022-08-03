ff5c7e658e965c71a95ec52003f1033eee2059e7
El Linzer <elinzer5@gmail.com>
2022-08-03 16:19:34 -0500

    edit a comment

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -8,7 +8,45 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//edit a comment
+router.put('/:commentId', requireAuth, async (req, res) => {
+    const userId = req.user.id;
+    const commentId = req.params.commentId;
+    const { body } = req.body;
 
+    const commentToEdit = await Comment.findByPk(commentId);
+
+    if (!commentToEdit) {
+        res.statusCode = 404;
+        res.json({
+            "message": "Comment couldn't be found",
+            "statusCode": 404
+          })
+    } else if (!body) {
+        res.statusCode = 400;
+        res.json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": {
+              "body": "Comment body text is required",
+            }
+          })
+    } else if (userId !== commentToEdit.userId) {
+        res.statusCode = 403;
+        res.json({
+            "message": "This is not your comment to edit!",
+            "statusCode": 403
+        })
+    } else {
+        commentToEdit.set({
+            body: body
+        })
+        await commentToEdit.save();
+
+        res.json(commentToEdit)
+    }
+
+})
 
 
 
