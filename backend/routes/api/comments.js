057f7d06ffd9c6fe0b8c9792c819dd31bc1c2a80
El Linzer <elinzer5@gmail.com>
2022-08-03 16:03:40 -0500

    made comments router

---
+++ backend/routes/api/comments.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User, Song, Album, Comment } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const songsRouter = require('./songs.js');
 const albumsRouter = require('./albums.js');
+const commentsRouter = require('./comments');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
@@ -21,6 +22,8 @@ router.use('/artists', usersRouter);
 router.use('/songs', songsRouter);
 
 router.use('/albums', albumsRouter);
+
+router.use('/comments', commentsRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
