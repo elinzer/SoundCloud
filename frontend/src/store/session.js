5e6cde2f22e56e65846ff5828a064bf20e25bb51
El Linzer <elinzer5@gmail.com>
2022-08-19 12:30:24 -0500

    finished phase 1

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -7,33 +7,41 @@ const REMOVE_USER = 'session/removeUser';
 
 //actions
 const setUser = (user) => {
-    return {
-      type: SET_USER,
-      payload: user,
-    };
+  return {
+    type: SET_USER,
+    payload: user,
   };
+};
 
-  const removeUser = () => {
-    return {
-      type: REMOVE_USER,
-    };
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
   };
+};
 
 //thunks
-  export const login = (user) => async (dispatch) => {
-    const { credential, password } = user;
-    const response = await csrfFetch('/api/session', {
-      method: 'POST',
-      body: JSON.stringify({
-        credential,
-        password,
-      }),
-    });
-    const data = await response.json();
-    dispatch(setUser(data));
-    return response;
-  };
+//login user
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
 
+//restore session
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
 
 
 const initialState = { user: null };
