91f959e86dc11ab6596b475699d0f0f88518c695
El Linzer <elinzer5@gmail.com>
2022-08-19 16:04:07 -0500

    add ugly navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
 import * as sessionActions from './store/session';
-
+import Navigation from './components/Navigation';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,6 +14,8 @@ function App() {
   }, [dispatch]);
 
   return isLoaded && (
+    <>
+    <Navigation isLoaded={isLoaded}/>
     <Switch>
       <Route path="/login">
         <LoginFormPage />
@@ -22,6 +24,7 @@ function App() {
       <SignupFormPage />
       </Route>
     </Switch>
+    </>
   );
 }
 
