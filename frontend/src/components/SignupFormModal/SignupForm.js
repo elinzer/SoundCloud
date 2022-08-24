2ebf06a8545a0ef9e653ecfb6b0afbd878453fd1
El Linzer <elinzer5@gmail.com>
2022-08-24 18:26:24 -0500

    sign up errors

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
@@ -12,27 +12,42 @@ function SignupForm() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [errors, setErrors] = useState([]);
+  const [validationErrors, setValidationErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  // if (sessionUser) return <Redirect to="/" />;
+
+  useEffect(() => {
+    const errors = [];
+    if (!email.includes('@')) errors.push('Email must be valid email address');
+    if (email.length < 3) errors.push('Email must be valid email address');
+    if (!email.includes('.com') || !email.includes('.co') || !email.includes('.io') || !email.includes('.net') || !email.includes('.')) errors.push('Email must be valid email')
+    if (username.includes('@')) errors.push('Username cannot be an email address');
+    if (username.length < 4) errors.push('Username must be at least 4 characters');
+    if (password.length < 6 && confirmPassword < 6) errors.push('Password must be at least 6 characters');
+    if (password != confirmPassword) errors.push('Confirm password must equal password');
+    setValidationErrors(errors);
+  }, [email, username, password])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(sessionActions.signUp({ firstName, lastName, email, username, password }))
+
+    if (validationErrors.length) {
+      return alert('Cannot Submit');
+    } else {
+      dispatch(sessionActions.signUp({ firstName, lastName, email, username, password }))
         .catch(async (res) => {
           const data = await res.json();
-          if (data && errors) setErrors(errors);
+          if (data && validationErrors) setValidationErrors(validationErrors);
         });
+
     }
-    return setErrors(['Confirm Password field must be the same as the Password field']);
+
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        {validationErrors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
       <label>
         First Name
