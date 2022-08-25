d1a7c95633711dc27ac4866d57d4a637a074101e
El Linzer <elinzer5@gmail.com>
2022-08-25 09:17:55 -0500

    more error handling

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -1,31 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
 function SignupForm() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
+
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [validationErrors, setValidationErrors] = useState([]);
   const [hasSubmitted, setHasSubmitted] = useState(false)
 
-  // if (sessionUser) return <Redirect to="/" />;
-
   useEffect(() => {
     const errors = [];
-    if (!email.includes('@')) errors.push('Email must be valid email 1');
-    if (email.length < 3) errors.push('Email must be valid email 2');
-    if (!(email.includes('.com') || email.includes('.co') || email.includes('.io') || email.includes('.net'))) errors.push('Email must be valid email 3')
+    if (email.length < 3 || !(email.includes('.com') || email.includes('.co') || email.includes('.io') || email.includes('.net'))) errors.push('Email must be valid email');
     if (username.includes('@')) errors.push('Username cannot be an email address');
     if (username.length < 4) errors.push('Username must be at least 4 characters');
     if (password.length < 6) errors.push('Password must be at least 6 characters');
-    if (confirmPassword != password) errors.push('Confirm password must equal password');
+
     setValidationErrors(errors);
   }, [email, username, password])
 
@@ -96,15 +90,6 @@ function SignupForm() {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
         </label>
