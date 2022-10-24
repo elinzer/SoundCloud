2f517b0a231382c91c8dd4e6254566d8c3a01f28
El Linzer <elinzer5@gmail.com>
2022-10-24 16:16:59 -0500

    got a functional carousel

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import DisplayAllSongs from "./SongDisplay";
 import bgImage from '../../images/Screen Shot 2022-08-25 at 8.14.10 PM.png';
 import '../../css/HomePage.css';
+import Carousel from "../Carousel/Carousel";
 
 const HomePage = ({ audioProp }) => {
 
@@ -24,7 +25,13 @@ const HomePage = ({ audioProp }) => {
     } else {
         sessionHomePage = (
             <div className="container">
-                <img className="splash-page-bg" src={bgImage} alt='a concert'></img>
+                <div className="carousel-con">
+                <Carousel>
+                    <img style={{height: '465px'}} src="https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_a-3bb2d420.jpg" alt=''></img>
+                    <img src="https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_featured_artists-8081257b.jpg" alt=''></img>
+                </Carousel>
+                </div>
+                {/* <img className="splash-page-bg" src={bgImage} alt='a concert'></img> */}
                 <div>
                     <h3>Explore what's popular today:</h3>
                     <div>
