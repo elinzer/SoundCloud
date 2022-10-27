eccf23b2dc0561e185c8af220f27a0dbd1b6b0b4
El Linzer <elinzer5@gmail.com>
2022-10-27 11:42:47 -0500

    new audio player in progress

---
+++ frontend/src/components/Player/Player.js
@@ -0,0 +1,19 @@
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import '../../css/AudioPlayer.css';
+
+const Player = (audioProp) => {
+    const [songAudio, setSongAudio] = audioProp.audioProp;
+
+    return (
+        <div className='audio-container'>
+            <AudioPlayer
+                className={songAudio === '' ? 'no-audio' : 'audio-player'}
+                autoPlay
+                src={songAudio}
+            />
+        </div>
+    );
+}
+
+export default Player;
